e400901ccf50a94d2aadd44e10659a157ec28a38
JaydeGorton1992 <116609587+JaydeGorton1992@users.noreply.github.com>
2023-04-05 10:21:55 +1000

    Add files via upload

---
+++ u3158046-Website-New Web Contexts (9393)/javascript/script.js
@@ -0,0 +1,14 @@
+
+function bookingClicked() {
+  var map = document.getElementById("map");
+  map.style.background = "url(/images/bookings.jpg)";
+  map.style.backgroundRepeat = "no-repeat";
+  map.style.backgroundSize = "contain";
+}
+
+function playgroundClicked() {
+  var map = document.getElementById("map");
+  map.style.background = "url(/images/facilities.jpg)";
+  map.style.backgroundRepeat = "no-repeat";
+  map.style.backgroundSize = "contain";
+}
